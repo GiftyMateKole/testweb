1b428adeeb06c042744663b7940607f00bec5845
matekuor <matekuor94@gmail.com>
2018-10-24 15:43:25 +0100

    a

---
+++ config/auth.js
@@ -0,0 +1,26 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '976738042535147', // your App ID
+        'clientSecret'  : 'c2d05c785852f34475d045b9efe0a046', // your App Secret
+        'callbackURL'   : 'https://mlab.com/databases/first_mongodb/collections/users',
+        'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'VRhLUReocCEvevbL8ckBONoGh',
+        'consumerSecret'    : 'Ix8zkcOWd4fJ5sNa4OaLKeG3JUtQ8ZcwAdb2gDZJph74cnWQw9',
+        'callbackURL'       : 'http://127.0.0.1:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://kuor:matekuor94@ds121950.mlab.com:21950/first_mongodb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
